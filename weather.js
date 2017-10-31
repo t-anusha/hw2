bcb3f7cfc41646ec28f8f2b94ac7534b1fdf795f
t-anusha <anusha.tomar@kellogg.northwestern.edu>
2017-10-30 20:01:43 -0500

    Part 1 included as comments

--- weather.js
+++ weather.js
@@ -15,7 +15,7 @@ let convertToJSON = function(response) {
 }
 
 let updateWeather = function(dataFromService) {
-  // console.info(dataFromService)
+  console.info(dataFromService)
 
   // City name
   city = dataFromService.name;
@@ -47,3 +47,44 @@ link.addEventListener("click", function(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(getWeather);
 });
+
+// Hard-coded Chicago data
+// let getWeather = function() {
+//   let latitude = '41.8781';
+//   let longitude = '-87.6298';
+//   let openweathermap_api_url = 'https://api.openweathermap.org/data/2.5/weather?'
+//   openweathermap_api_url += 'lat=' + latitude
+//   openweathermap_api_url += '&lon=' + longitude
+//   openweathermap_api_url +='&appid=4ce6f502d38ddae567bf1702b05e168c&units=imperial'
+//
+//   fetch(openweathermap_api_url).then(convertToJSON).then(updateWeather).catch(displayError);
+//  }
+// let convertToJSON = function(response) {
+//   return response.json();
+// }
+//
+// let updateWeather = function(dataFromService) {
+//   console.info(dataFromService)
+//
+//   city = dataFromService.name;
+//   console.info(city)
+//   let city_div = document.getElementById("get_city");
+//   city_div.innerHTML = city;
+//
+//   temperature = dataFromService.main.temp.toFixed(0);
+//   console.info(temperature)
+//   let temp_div = document.getElementById("get_temp");
+//   temp_div.innerHTML = "It is " + temperature + " degrees outside"
+// }
+//
+// let displayError = function(error) {
+//   console.debug(error);
+//   window.alert("Sorry, something went wrong.");
+// }
+//
+// let link = document.getElementById("get_forecast")
+// link.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     getWeather();
+//     navigator.geolocation.getCurrentPosition(handlePosition);
+// })
